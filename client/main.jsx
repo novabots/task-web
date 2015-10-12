a45d56cf4754bfc5983ba59cd9ed73e46f79c584
Ian Hilt <ian@novacreative.com>
2015-10-12 12:42:07 -0400

    Revert "Disable dropping task from current users area to current users area"
    This reverts commit 5084180fb89fd50820504d820e8bb22a011b55a4.

--- client/main.jsx
+++ client/main.jsx
@@ -230,17 +230,14 @@ Template.taskIcon.onRendered(function () {
     });
     $(".droppable").droppable({
         drop: function( event, ui ) {
-            var isUser = Meteor.users.findOne(event.target.id);
-            if (typeof isUser !== "undefined" && event.target.id !== Meteor.userId()) {
-                Meteor.call("changeTaskOwner", event.target.id, ui.draggable[0].id, function(err, res){
-                    if(res) {
-                        toastr.success('You have updated the task.');
-                    }
-                    if(err) {
-                        toastr.error('Error: You have not updated the task.');
-                    }
-                });
-            }
+            Meteor.call("changeTaskOwner", event.target.id, ui.draggable[0].id, function(err, res){
+                if(res) {
+                    toastr.success('You have updated the task.');
+                }
+                if(err) {
+                    toastr.error('Error: You have not updated the task.');
+                }
+            })
         }
     });
 });
