e9f4a78bc541faa378a7d738500fb45fecd09d95
Dave Ramsington <grokbot.dwr@gmail.com>
2016-01-18 09:10:15 -0500

    Added proper zoom functionality

--- modules/app/client/components/organization.jsx
+++ modules/app/client/components/organization.jsx
@@ -105,7 +105,6 @@ export class Organization extends Component {
     }
 }
 
-
 export class OrganizationGraph extends Component {
     constructor(props){
         super(props);
@@ -113,12 +112,18 @@ export class OrganizationGraph extends Component {
             creatingTeam : false,
             zoom : 1,
             width : 0,
-            height: 0
+            height: 0,
+            transMatrix: [1,0,0,1,0,0],
+            scale: 1.0,
+            scaleInc: 0.1,
+            lastZoom: null
         };
         this.creatingTeam = this.creatingTeam.bind(this);
         this.toggleTeamForm = this.toggleTeamForm.bind(this);
         this.updateDimensions = this.updateDimensions.bind(this);
         this.updateDimensions = _.debounce(this.updateDimensions,1000);
+        this.handleScroll = this.handleScroll.bind(this);
+        this.zoom = this.zoom.bind(this);
     }
     creatingTeam(){
         this.setState({ "creatingTeam" : true })
@@ -173,11 +178,53 @@ export class OrganizationGraph extends Component {
         });
     }
 
-    handleZoom(e) {
-        console.log(e.deltaY);
+    handleScroll(e) {
+        let scroll = e.deltaY.toFixed(3);
+        let zoomDir = (scroll > 0);
+        let lastZoom = this.state.lastZoom;
+        if(lastZoom == null) {
+            lastZoom = !zoomDir;
+        }
+        this.zoom(zoomDir, lastZoom);
+        this.setState({lastZoom: zoomDir});
+    }
+
+    zoom(zoom, last){
+        let newScale = 0;
+        let scale = this.state.scale;
+        let inc = this.state.scaleInc;
+        let width = this.state.width;
+        let height = this.state.height;
+        let transMatrix = [1,0,0,1,0,0];
+        if(zoom) {
+            newScale = scale + inc;
+            if(newScale > 2) {
+                newScale = 2;
+            }
+        } else {
+            newScale = scale - inc;
+            if(newScale < 0.5) {
+                newScale = 0.5;
+            }
+        }
+        console.log(transMatrix);
+        console.log(newScale);
+        if((newScale != scale && zoom == last) || (newScale == scale && zoom != last)) {
+            for (var i=0; i<transMatrix.length; i++) {
+                transMatrix[i] *= newScale;
+            }
+            transMatrix[4] += (1-newScale)*width/2;
+            transMatrix[5] += (1-newScale)*height/2;
+            this.setState({transMatrix: transMatrix, scale: newScale});
+        }
+    }
+
+    pan(e) {
+        console.log(e);
     }
 
     render() {
+        let matrix = this.state.transMatrix.join(" ");
         return (
             <div>
                 <div id="org" className="well well-lg">
@@ -193,8 +240,10 @@ export class OrganizationGraph extends Component {
                 </div>
                 <div className="org-teams">
                 {this.props.teams ?
-                    <svg onWheel={this.handleZoom} id="org-graph" width="100%" height={this.state.height} ref={(ref) => this.canvas = ref} viewBox={"0 0 " + this.state.width + " " + this.state.height}>
-                        {this.renderTeams(this.state.zoom)}
+                    <svg onWheel={this.handleScroll} id="org-graph" width="100%" height={this.state.height} ref={(ref) => this.canvas = ref} viewBox={"0 0 " + this.state.width + " " + this.state.height}>
+                        <g id="transform-matrix" transform={"matrix("+matrix+")"}>
+                            {this.renderTeams(this.state.zoom)}
+                        </g>
                     </svg>
                     :
                     <div className="loading-indicator"><i className="fa fa-circle-o-notch fa-spin"></i></div>
